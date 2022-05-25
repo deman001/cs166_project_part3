394644bb0a5e174e5fe2dcb83b7f0d00d82fafe6
Cody Kurpanek <codykurpanek@gmail.com>
2022-05-25 02:10:37 -0700

    Modified sql scripts folder so it is easier to start up postgre and create the Cafe database

--- sql/scripts/create_and_initialize_db.sh
+++ sql/scripts/create_and_initialize_db.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "creating db named ... "$USER"_DB"
+createdb -h localhost -p $PGPORT $USER"_DB"
+pg_ctl status
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 psql -h localhost -p $PGPORT $USER"_DB" < $DIR/../src/create_tables.sql
 psql -h localhost -p $PGPORT $USER"_DB" < $DIR/../src/create_indexes.sql
