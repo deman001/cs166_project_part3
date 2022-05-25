694b5ebd90eb1cbadb88c01118bb94274d47902f
Cody Kurpanek <codykurpanek@gmail.com>
2022-05-25 01:52:04 -0700

    added a template for Dale to also load data using his path

--- sql/src/load_data.sql
+++ sql/src/load_data.sql
@@ -1,17 +1,37 @@
+
 COPY MENU
-FROM 'menu.csv'
+FROM '/extra/ckurp001/cs166/cs166_project/data/menu.csv'
 WITH DELIMITER ';';
 
 COPY USERS
-FROM 'users.csv'
+FROM '/extra/ckurp001/cs166/cs166_project/data/users.csv'
 WITH DELIMITER ';';
 
 COPY ORDERS
-FROM 'orders.csv'
+FROM '/extra/ckurp001/cs166/cs166_project/data/orders.csv'
 WITH DELIMITER ';';
 ALTER SEQUENCE orders_orderid_seq RESTART 87257;
 
 COPY ITEMSTATUS
-FROM 'itemStatus.csv'
+FROM '/extra/ckurp001/cs166/cs166_project/data/itemStatus.csv'
+WITH DELIMITER ';';
+
+
+
+COPY MENU
+FROM 'YOUR PATH HERE/data/menu.csv'
+WITH DELIMITER ';';
+
+COPY USERS
+FROM 'YOUR PATH HERE/data/users.csv'
+WITH DELIMITER ';';
+
+COPY ORDERS
+FROM 'YOUR PATH HERE/data/orders.csv'
+WITH DELIMITER ';';
+ALTER SEQUENCE orders_orderid_seq RESTART 87257;
+
+COPY ITEMSTATUS
+FROM 'YOUR PATH HERE/data/itemStatus.csv'
 WITH DELIMITER ';';
 
