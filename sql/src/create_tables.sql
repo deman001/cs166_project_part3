4b5eb2d1fef1c995455b8443e6b658b24bf26cdf
Dale Eman <deman001@wch135-10.cs.ucr.edu>
2022-06-02 02:03:43 -0700

    added placesOrder, favoriteItems, and working on creating the inheritence

--- sql/src/create_tables.sql
+++ sql/src/create_tables.sql
@@ -2,13 +2,14 @@ DROP TABLE Users CASCADE;
 DROP TABLE Orders CASCADE;
 DROP TABLE Menu CASCADE;
 DROP TABLE ItemStatus;
+DROP TYPE IF EXISTS UserType AS ENUM('Manager', 'Employee', 'User');
 
 CREATE TABLE Users(
 	login char(50) UNIQUE NOT NULL, 
 	phoneNum char(16) UNIQUE, 
 	password char(50) NOT NULL,
 	favItems char(400),
-	type char(8) NOT NULL,
+	type UserType NOT NULL,
 	PRIMARY KEY(login));
 
 CREATE TABLE Menu(
@@ -36,3 +37,21 @@ CREATE TABLE ItemStatus(
 	PRIMARY KEY(orderid,itemName),
 	FOREIGN KEY(orderid) REFERENCES Orders(orderid),
 	FOREIGN KEY(itemName) REFERENCES Menu(itemName));
+
+CREATE TABLE placesOrder(
+	orderid integer UNIQUE NOT NULL,
+	itemName char(30) NOT NULL,
+	PRIMARY KEY(orderid, itemName),
+	FOREIGN KEY(orderid) REFERENCES Orders(orderid),
+	FOREIGN KEY(itemName) REFERENCES Menu(itemName));
+
+CREATE TABLE favoriteItem(
+	login char(50) UNIQUE NOT NULL,
+	itemName char(30) NOT NULL,
+	PRIMARY KEY(login, itemName),
+	FOREIGN KEY(login) REFERENCES Users(login),
+	FOREIGN KEY(itemName) REFERENCES Menu(itemName));
+
+ALTER TABLE Users
+ADD CONSTRAINT employeeConstrant
+FOREIGN KEY(
